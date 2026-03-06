817b8ce00a5554b0e24154d0f47c9d92b6b58d4f
eduardoDC101 <edutnb2018@gmail.com>
2026-03-06 09:36:00 -0300

    Praticando 05 PHP

---
+++ Praticando05/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Menu - Praticando 05</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+        <style>
+            nav div:hover{
+                background-color:#8cc3a9;
+                cursor: pointer;
+            }
+        </style>
+    </head>
+    <body class="w-100 vh-100">
+        <section class="w-100 px-5 py-3">
+            <h1>Menu Praticandos de Loop</h1>
+            <hr>
+            <header class="w-100">
+                <nav class="w-100 d-flex flex-column border border-1 border-gray-300 rounded-2">
+                    <div class="border border-1 border-gray-300 px-3 py-2">1. <a href="tabuada.php">Praticando Tabuada</a></div>
+                    <div class="border border-1 border-gray-300 px-3 py-2">2. <a href="#">Praticando Contador</a></div>
+                    <div class="border border-1 border-gray-300 px-3 py-2">3. <a href="#">Praticando Números Primos</a></div>
+                    <div class="border border-1 border-gray-300 px-3 py-2">4. <a href="#">Praticando Gerador de Tabela</a></div>
+                </nav>
+            </header>
+        </section>
+    </body>
+</html>
---
+++ Praticando05/tabuada.php
@@ -0,0 +1,47 @@
+<?php 
+
+$numero = $_GET['numero'] ?? 0;
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tabuada - Praticando 05</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+        <style>
+            input{
+                width: 150px;
+            }
+        </style>
+    </head>
+    <body class="w-100 vh-100">
+        <section class="w-100 px-5 py-3">
+            <h1>Tabela</h1>
+            <hr>
+            <form action="tabuada.php" method="GET" class="w-25 d-flex flex-column gap-3 align-items-end">
+                <label for="numero">
+                    Numero: 
+                    <input class="mx-2" type="number" name="numero">
+                </label>
+                <div id="botoes">
+                    <button type="submit" class="text-white bg-success border-0 rounded-1 px-3 py-2">Enviar</button>
+                    <button type="reset"  class="bg-warning border-0 rounded-1 px-3 py-2">Limpar</button>
+                </div>
+            </form>
+
+            <?php 
+            if($numero != ""){
+                echo "<hr>";
+                echo "<h2>Tabuada do $numero</h2>";
+                for($i = 1; $i < 11; $i++){
+                    echo "<p>$numero x $i = " . ($numero * $i) . "</p>";
+                }
+            }
+            ?>
+
+        </section>
+    </body>
+</html>
